0e63f57eb1b45fb1f62813dcac4fbb672285a9ad
Godsheritage D Adeoye <Adeoyegodsheritage@gmail.com>
2022-01-08 16:19:12 +0100

    This is my first commit

---
+++ js/main.js
@@ -0,0 +1,45 @@
+const section = document.querySelector('section');
+const lists = document.querySelector('.listss');
+const h1 = document.createElement('h1');
+const input = document.createElement('input');
+const ol = document.createElement('ol')
+
+
+
+h1.innerText = "To-Do list";
+h1.classList.add("h-style");
+section.appendChild(h1);
+
+
+input.setAttribute('placeholder', 'To-Do')
+input.setAttribute('type', 'text')
+input.classList.add('form-control')
+section.appendChild(input);
+
+
+lists.appendChild(ol)
+
+// to get the input and add the listener
+input.addEventListener('keypress',function(e){
+    if(e.key === 'Enter'){
+        const li = document.createElement('li')
+        li.setAttribute('id','list-items')
+        ol.appendChild(li)
+        li.innerText = this.value;
+        this.value = '';
+    }
+   
+})
+
+// const lists = document.querySelector('#list-items');
+// lists.addEventListener('click', () => {
+//     lists.style.list-style('underline');
+
+// }) 
+
+// this.li.addEventListener('click', e => {
+//     console.log(this);
+// })\\
+
+
+
